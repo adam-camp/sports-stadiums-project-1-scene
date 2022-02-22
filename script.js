0315853967c52e47d79795a92ea9726cfb47e29d
adam-camp <98061122+adam-camp@users.noreply.github.com>
2022-02-21 21:57:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-require(["esri/Map", "esri/views/SceneView"], function (Map, SceneView) {
-        var map = new Map({
-          basemap: "topo-vector",
-          ground: "world-elevation"
-        });
-
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          scale: 50000000,
-          center: [15, 65] // longitude, latitude
-        });
+require([
+  "esri/WebScene",
+  "esri/views/SceneView",
+  "esri/Camera",
+  "esri/widgets/Home",
+  "esri/widgets/Search",
+  "esri/widgets/Legend",
+      "dojo/domReady!"        
+     ], function(WebScene, SceneView, Camera, Home, Search, Legend) {
+ var scene = new WebScene({
+        portalItem: { 
+          id: "fc5f67fceaa9496086cf86b9519d4423"
+        }
       });
+  //*fc5f67fceaa9496086cf86b9519d4423*//
+  
+   var camera = new Camera({
+        position: [         
+          -101.1994, // lon
+          43.78, // lat
+          5000000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      });
+
+var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: camera
+            });
+      
+  var homeBtn = new Home({
+        view: view
+      });
+  view.ui.add(homeBtn, "top-right");
+       
+});
